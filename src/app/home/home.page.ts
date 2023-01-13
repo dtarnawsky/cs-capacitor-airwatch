a06fff149b93f89cefb5dc4c6de97a2fa37ee01b
Damian Tarnawsky <damian@ionic.io>
2023-01-13 09:40:46 -0800

    Add Capacitor Preferences

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AirwatchService } from '../airwatch.service';
+import { Preferences } from '@capacitor/preferences';
 
 @Component({
   selector: 'app-home',
@@ -9,9 +10,11 @@ import { AirwatchService } from '../airwatch.service';
 export class HomePage {
 
   public vm = { status: '' };
+  public keys: Array<string> = [];
 
   constructor(private airWatch: AirwatchService) {
     this.vm.status = airWatch.status;
+    this.initAppConfig();
   }
 
   public async initialize() {
@@ -19,6 +22,20 @@ export class HomePage {
   }
 
   public async getUser() {
-    alert( await this.airWatch.getUser());
+    alert(await this.airWatch.getUser());
+  }
+
+  public async getConfig() {
+    const result = await Preferences.get({ key: 'ionic.email' });
+    alert(result.value);
+  }
+  public async get(keyName: string) {
+    const result = await Preferences.get({ key: keyName });
+    alert(result.value);
+  }
+
+  private async initAppConfig() {
+    await Preferences.configure({ group: 'NativeStorage' });
+    this.keys = (await Preferences.keys()).keys;
   }
 }
